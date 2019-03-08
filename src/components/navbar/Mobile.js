82aeaef13f1200f56ba57d1d7232321f3988e198
Ashkaan Jaberi <ashkaan14@gmail.com>
2019-03-07 16:04:14 -0800

    mobile nav sliding in and out

--- src/components/navbar/Mobile.js
+++ src/components/navbar/Mobile.js
@@ -3,17 +3,28 @@ import React, {Component} from 'react';
 
 class Mobile extends Component {
   state = {
-    showMenu: false,
+    showMenu: true,
   }
   toggleMenu = () => {
     this.setState({showMenu: !this.state.showMenu})
   }
   render() {
+    let className = this.state.showMenu ? 'mobile-menu show' : 'mobile-menu hide'
     return (
-      <div className='mobile-nav'>
-        <h1> Vision Clear </h1>
-        <div onClick={this.toggleMenu}> {this.state.showMenu ? 'X' : 'O'} </div>
-      </div>
+      <React.Fragment>
+        <div className='mobile-nav'>
+          <h1> Vision Clear </h1>
+          <div onClick={this.toggleMenu}> {this.state.showMenu ? 'X' : 'O'} </div>
+        </div>
+
+        <ul className={className}>
+          <li className='active'> Home </li>
+          <li> About </li>
+          <li> Interviews </li>
+          <li> Blog </li>
+        </ul>
+
+      </ React.Fragment>
     )
   }
 }
